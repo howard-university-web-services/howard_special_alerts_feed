34e5348612bcc04a4d8f55fa49b4c439e5792a79
Dan Rogers <dan.rogers@idfive.com>
2020-01-13 09:10:37 -0500

    module formatting and minor cleanup

--- README.md
+++ README.md
@@ -1,42 +1,34 @@
-INTRODUCTION
-------------
+# Howard Special Alerts Feed
 
 This module is designed to display special alerts for Howard University, on any
 Drupal site. It creates a block, which can then be placed in whatever region you
 desire. This module relies on a handlebars template, which then renders the HTML.
 
-REQUIREMENTS
-------------
+## Requirements
 
 This module requires the following modules:
 
-* block
+- block
 
-INSTALLATION AND UPDATES
-------------
+## Installation and updates
 
-**Install Via Composer:**
-```
-composer install howard/howard_special_alerts_feed
-```
+### Install Via Composer
 
-**Update Via Composer:**
-```
-composer update howard/howard_special_alerts_feed
-```
+- `composer install howard/howard_special_alerts_feed`
 
-CONFIGURATION
--------------
+### Update Via Composer
 
-* Configure user permissions in
+- `composer update howard/howard_special_alerts_feed`
+
+## Configuration
+
+- Configure user permissions in
   Structure » Block Layout » Howard Special Alerts Feed:
   (admin/structure/block/manage/howardspecialalertsfeed)
-  - Site URL: This is the URL of the MASTER install, likely http://newsroom.howard.edu
-    unless otherwise specified.
+- Site URL: This is the URL of the MASTER install, likely http://newsroom.howard.edu
+  unless otherwise specified.
 
-
-TROUBLESHOOTING
----------------
+## Troubleshooting
 
 If you are running into errors, be sure to check that CORS is running on the
 MASTER install, and that you can ping your endpoints.
--- src/Plugin/Block/SpecialAlertsFeedBlock.php
+++ src/Plugin/Block/SpecialAlertsFeedBlock.php
@@ -6,7 +6,7 @@ use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Form\FormStateInterface;
 
 /**
- * Special alerts feed block
+ * Special alerts feed block.
  *
  * @Block(
  *   id = "special_alerts_feed",
@@ -19,45 +19,47 @@ class SpecialAlertsFeedBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function defaultConfiguration() {
-    return array(
+    return [
       'special_alerts_feed_settings_site_url' => '',
-    );
+    ];
   }
 
   /**
    * {@inheritdoc}
    */
   public function blockForm($form, FormStateInterface $form_state) {
+
     // General info/help text.
-    $form['info_text'] = array(
+    $form['info_text'] = [
       '#markup' => '
       <p>This block is a feed of Special Alerts, fed from the Howard Newsroom.
       It displays time sensitive University Information.</p>
       <p>See the <a href="/admin/help/howard_special_alerts_feed">Help Section</a>
-      for more information.</p>'
-    );
-    //General settings fieldset.
-    $form['special_alerts_feed_settings'] = array(
+      for more information.</p>',
+    ];
+
+    // General settings fieldset.
+    $form['special_alerts_feed_settings'] = [
       '#type' => 'fieldset',
       '#title' => $this->t('Special Alerts Feed Settings'),
-    );
+    ];
 
-    //URL of the feed API.
-    $form['special_alerts_feed_settings']['special_alerts_feed_settings_environment'] = array(
+    // URL of the feed API.
+    $form['special_alerts_feed_settings']['special_alerts_feed_settings_environment'] = [
       '#type' => 'select',
       '#title' => $this->t('Alert Environment'),
       '#default_value' => $this->configuration['special_alerts_feed_settings_environment'],
       '#options' => [
         'https://newsroom.howard.edu' => $this
           ->t('Production'),
-          'http://stg.newsroom.howard.edu' => $this
+        'http://stg.newsroom.howard.edu' => $this
           ->t('Staging'),
         'http://dev.newsroom.howard.edu' => $this
           ->t('Development'),
       ],
       '#description' => $this->t('Which Howard Newsroom environment to pull from. Most times, this should be production.'),
-      '#required' => TRUE
-    );
+      '#required' => TRUE,
+    ];
 
     return $form;
   }
@@ -73,15 +75,15 @@ class SpecialAlertsFeedBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function build() {
-    $build = array();
-    $build['content'] = array(
+    $build = [];
+    $build['content'] = [
       '#theme' => 'howard_special_alerts_feed_templating',
-    );
+    ];
     $build['#attached']['library'][] = 'howard_special_alerts_feed/howard_special_alerts_feed.alerts_feed';
-    $build['#attached']['drupalSettings']['howard_special_alerts_feed'] = array(
+    $build['#attached']['drupalSettings']['howard_special_alerts_feed'] = [
       'pathToAlertsFeedList' => $this->configuration['special_alerts_feed_settings_environment'],
       'pathToAlertsFeedModule' => drupal_get_path('module', 'howard_special_alerts_feed'),
-    );
+    ];
     return $build;
   }
 
