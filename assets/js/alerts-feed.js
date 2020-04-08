36ad7dc92b343e4b835dbc6619ac71b19431d1aa
Michael Craven <michael.craven@idfive.com>
2020-04-08 18:38:28 -0400

    Fix to add class to body.

--- assets/js/alerts-feed.js
+++ assets/js/alerts-feed.js
@@ -1,71 +1,60 @@
 /**
- * @file
- * Contains the definition of the behaviour specialAlertsFeed.
+ * Created by Dan on 12/11/15.
+ * Runs the meat and potatoes of getting the div, getting the JSON, and applying handlebars magic.
  */
+(function ($) {
 
-(function ($, Drupal, drupalSettings) {
+  var EVENT_METHOD = {
 
-  'use strict';
-  /**
-   * Attaches the specialAlertsFeed behavior.
-   */
-  Drupal.behaviors.specialAlertsFeed = {
-    attach: function (context, settings) {
-      var EVENT_METHOD ={
-        handlerData : function(templateInput){
-          var source;
-          var template;
-          var path = '/' + drupalSettings.howard_special_alerts_feed.pathToAlertsFeedModule + '/assets/templates/alerts-handlebars-template.html'
-          $.ajax({
-            url: path,
-            cache: false,
-            success: function (data) {
-              var template = Handlebars.compile(data);
-              var eventHTML = template(templateInput);
-              EVENT_METHOD.renderEvents(eventHTML);
-            }
-          });
-        },
+    handlerData: function (templateInput) {
 
-        loadEventData : function(){
-          // Get todays date, and format to yyyy-mm-dd
-          var fullDate = new Date();
-          var twoDigitMonth = (fullDate.getMonth()+1)+"";if(twoDigitMonth.length==1)	twoDigitMonth="0" +twoDigitMonth;
-          var twoDigitDate = fullDate.getDate()+"";if(twoDigitDate.length==1)	twoDigitDate="0" +twoDigitDate;
-          var currentDate = fullDate.getFullYear() + "-" + twoDigitMonth + "-" + twoDigitDate;
-
-          // Get todays time, and format to 00:00:00
-          var hours = fullDate.getUTCHours();
-          var minutes = fullDate.getUTCMinutes();
-          var currentTime = hours + ":" + minutes + ":00";
-          var path = drupalSettings.howard_special_alerts_feed.pathToAlertsFeedList + "/api/alerts?filter[start_date][value]=" + currentDate + "%20" + currentTime + "&filter[start_date][operator]='<='&filter[end_date][value]=" + currentDate + "%20" + currentTime + "&filter[end_date][operator]='>='";
-
-          $.ajax({
-            url: path,
-            method: 'get',
-            success: function (data) {
-              if (data.data.length) {
-                for(var k in data.data) {
-                  data.data[k]['env'] = drupalSettings.howard_special_alerts_feed.pathToAlertsFeedList;
-                }
-                EVENT_METHOD.handlerData(data);
-                $('body').addClass('alerts-active');
-              } else {
-                $('.alerts-feed-wrapper').hide();
-                $('body').addClass('no-alerts');
-              }
-            }
-          });
-        },
-        renderEvents : function(eventHTML){
-          $('.alerts-feed-wrapper').html(eventHTML);
+      var source;
+      var template;
+      var path = '/' + Drupal.settings.howard_special_alerts_feed.pathToAlertsFeedModule + '/assets/templates/alerts-handlebars-template.html'
+      $.ajax({
+        url: path,
+        cache: false,
+        success: function (data) {
+          source = data;
+          template = Handlebars.compile(source);
+          eventHTML = template(templateInput);
+          EVENT_METHOD.renderEvents(eventHTML);
         }
-      };
-
-      $(document).ready(function(){
-        EVENT_METHOD.loadEventData();
       });
+    },
 
+    loadEventData: function () {
+
+      // Get todays date, and format to yyyy-mm-dd
+      var fullDate = new Date();
+      var twoDigitMonth = (fullDate.getMonth() + 1) + ""; if (twoDigitMonth.length == 1) twoDigitMonth = "0" + twoDigitMonth;
+      var twoDigitDate = fullDate.getDate() + ""; if (twoDigitDate.length == 1) twoDigitDate = "0" + twoDigitDate;
+      var currentDate = fullDate.getFullYear() + "-" + twoDigitMonth + "-" + twoDigitDate;
+
+      // Get todays time, and format to 00:00:00
+      var hours = fullDate.getUTCHours();
+      var minutes = fullDate.getUTCMinutes();
+      var currentTime = hours + ":" + minutes + ":00";
+
+      $.ajax({
+        url: Drupal.settings.howard_special_alerts_feed.pathToAlertsFeedList + "/api/alerts?filter[start_date][value]=" + currentDate + "%20" + currentTime + "&filter[start_date][operator]='<='&filter[end_date][value]=" + currentDate + "%20" + currentTime + "&filter[end_date][operator]='>='",
+        method: 'get',
+        success: function (data) {
+          EVENT_METHOD.handlerData(data);
+          //console.log(data);
+          $('body').addClass('alerts-presented-' + data.count);
+        }
+
+      })
+    },
+
+    renderEvents: function (eventHTML) {
+      $('.alerts-feed-wrapper').html(eventHTML);
     }
   };
-})(jQuery, Drupal, drupalSettings);
+
+  $(document).ready(function () {
+    EVENT_METHOD.loadEventData();
+  });
+
+}(jQuery));
