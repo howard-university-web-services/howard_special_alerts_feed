fe9f2f7e77d9bf10711915cd44ba8ab69bc39112
Dan Rogers <dan.rogers@idfive.com>
2020-07-27 08:05:08 -0600

    remove https from dev/stg as the certs dont resolve on acquia.

--- src/Plugin/Block/SpecialAlertsFeedBlock.php
+++ src/Plugin/Block/SpecialAlertsFeedBlock.php
@@ -52,9 +52,9 @@ class SpecialAlertsFeedBlock extends BlockBase {
       '#options' => [
         'https://newsroom.howard.edu' => $this
           ->t('Production'),
-        'https://stg.newsroom.howard.edu' => $this
+        'http://stg.newsroom.howard.edu' => $this
           ->t('Staging'),
-        'https://dev.newsroom.howard.edu' => $this
+        'http://dev.newsroom.howard.edu' => $this
           ->t('Development'),
       ],
       '#description' => $this->t('Which Howard Newsroom environment to pull from. Most times, this should be production.'),
