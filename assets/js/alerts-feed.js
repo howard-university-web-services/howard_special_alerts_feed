be85f069ea94358ae1c4b6aacc88a0dbb71502e1
Dan Rogers <dan.rogers@idfive.com>
2024-01-18 13:21:54 -0700

    Adjust and refactor JS to work better with D10

--- assets/js/alerts-feed.js
+++ assets/js/alerts-feed.js
@@ -3,98 +3,109 @@
  * Contains the definition of the behavior specialAlertsFeed.
  */
 
-(function ($, Drupal, drupalSettings) {
 
+(function () {
   'use strict';
-  /**
-   * Attaches the specialAlertsFeed behavior.
-   */
-  Drupal.behaviors.specialAlertsFeed = {
-    attach: function (context, settings) {
-      var EVENT_METHOD = {
-        handlerData: function (templateInput) {
-          var path = '/' + drupalSettings.howard_special_alerts_feed.pathToAlertsFeedModule + '/assets/templates/alerts-handlebars-template.html'
-          $.ajax({
-            url: path,
-            cache: false,
-            success: function (data) {
-              var template = Handlebars.compile(data);
-              var eventHTML = template(templateInput);
-              EVENT_METHOD.renderEvents(eventHTML);
-            }
-          });
-        },
-        loadEventData: function () {
-          // Set URL
-          var site = 'https://thedig.howard.edu';
 
-          // Get todays date, and format to ISO
-          var isoDate = new Date().toISOString();
-          var path = site + "/jsonapi/node/alert?filter[start-date][condition][path]=field_alert_start_date&filter[start-date][condition][value]=" + isoDate + "&filter[start-date][condition][operator]=%3C%3D&filter[end-date][condition][path]=field_alert_end_date&filter[end-date][condition][value]=" + isoDate + "&filter[end-date][condition][operator]=%3E%3D&filter[status][value]=1";
+  let specialAlertsFeed = {
+    alertBar: `<article id="" class=""> 
+                    <a class="newsroom-alert" href="">
+                        <strong></strong>
+                    </a>
+                    <a class="newsroom-alert-dismiss" data-alert-id="">
+                        <span class="hidden">Dismiss alert</span>
+                    </a>
+                </article>`,
 
-          $.ajax({
-            url: path,
-            method: 'get',
-            success: function (data) {
-              if (data.data.length) {
-                var alerts = [];
-                for (var k in data.data) {
-                  var cookie = EVENT_METHOD.getCookie('howard-newsroom-alerts--' + data.data[k]['id']);
-                  if (cookie == null) {
-                    // Set the chosen env to be available.
-                    data.data[k]['env'] = site;
-                    alerts.push(data.data[k]);
-                  }
-                }
-                data.data = alerts;
-                if (data.data.length) {
-                  EVENT_METHOD.handlerData(data);
-                } else {
-                  EVENT_METHOD.hideAlerts();
-                }
-              } else {
-                EVENT_METHOD.hideAlerts();
-              }
-            }
-          });
-        },
-        hideAlerts: function () {
-          $('.alerts-feed-wrapper').hide();
-          $('body').addClass('no-alerts');
-        },
-        renderEvents: function (eventHTML) {
-          // Add HTML.
-          $('body').addClass('alerts-active');
-          $('.alerts-feed-wrapper').html(eventHTML);
-          // Attach click functionality.
-          $(".newsroom-alert-dismiss").each(function () {
-            $(this).on("click", function () {
-              var alertId = $(this).attr("data-alert-id");
-              $('#newsroom-alert-' + alertId).addClass('hidden');
-              EVENT_METHOD.setCookie('howard-newsroom-alerts--' + alertId, 'Howard Dig Alert ' + alertId + ' dismissed.', 1);
-            });
-          });
-        },
-        setCookie: function (key, value, days) {
-          var expires = new Date();
-          if (days) {
-            expires.setTime(expires.getTime() + (days * 24 * 60 * 60 * 1000));
-            document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();
-          } else {
-            document.cookie = key + '=' + value + ';expires=Fri, 30 Dec 9999 23:59:59 GMT;';
-          }
-        },
-        getCookie: function (key) {
-          var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
-          return keyValue ? keyValue[2] : null;
+    setAlertBars: function (data) {
+
+      let alertBarWrapper = document.getElementById("howard_special_alerts_feed");
+      data.forEach(alert => {
+        let parser = new DOMParser();
+        let bar = parser.parseFromString(specialAlertsFeed.alertBar, "text/html");
+        let url = alert.env + alert.attributes.path.alias;
+
+        if (alert.attributes.field_alert_links.uri) {
+          url = alert.attributes.field_alert_links.uri;
         }
-      };
-
-      once('specialAlertsFeed', context).forEach(function (element) {
-        EVENT_METHOD.loadEventData();
+        bar.querySelector("article").setAttribute('id', "newsroom-alert-" + alert.id + "");
+        bar.querySelector("article").setAttribute('class', "newsroom-alert--" + alert.id + "");
+        bar.querySelector("article a.newsroom-alert").setAttribute('class', "newsroom-alert newsroom-alert--" + alert.attributes.field_alert_level + "");
+        bar.querySelector("article a.newsroom-alert").setAttribute('href', "" + url + "");
+        bar.querySelector("article a.newsroom-alert strong").innerHTML = alert.attributes.title;
+        if (alert.attributes.field_alert_subtitle) {
+          bar.querySelector("article a.newsroom-alert").textContent += "" + alert.attributes.field_alert_subtitle + "";
+        }
+        bar.querySelector("article a.newsroom-alert-dismiss").setAttribute("data-alert-id", alert.id);
+        bar.querySelector("article a.newsroom-alert-dismiss span").textContent += " " + alert.id;
+        const alertHtml = bar.querySelector('article');
+        alertBarWrapper.append(alertHtml);
       });
+      // Add body class
+      document.body.classList.add('alerts-active');
+      // Set dismiss functionality
+      specialAlertsFeed.setDismiss();
 
-
+    },
+    loadEventData: function () {
+      // Set URL
+      const site = 'https://thedig.howard.edu';
+      // Get todays date, and format to ISO
+      const isoDate = new Date().toISOString();
+      const path = site + "/jsonapi/node/alert?filter[start-date][condition][path]=field_alert_start_date&filter[start-date][condition][value]=" + isoDate + "&filter[start-date][condition][operator]=%3C%3D&filter[end-date][condition][path]=field_alert_end_date&filter[end-date][condition][value]=" + isoDate + "&filter[end-date][condition][operator]=%3E%3D&filter[status][value]=1";
+      fetch(path)
+        .then((response) => response.json())
+        .then((json) => specialAlertsFeed.checkAlerts(json.data, site))
+        .catch((err) => console.log(err));
+    },
+    checkAlerts: function (data, site) {
+      if (data.length) {
+        let alerts = [];
+        for (let k in data) {
+          let cookie = specialAlertsFeed.getCookie('howard-newsroom-alerts--' + data[k]['id']);
+          if (cookie == null) {
+            data[k]['env'] = site;
+            alerts.push(data[k]);
+          }
+        }
+        data = alerts;
+        if (data.length) {
+          specialAlertsFeed.setAlertBars(data);
+        } else {
+          specialAlertsFeed.hideAlerts();
+        }
+      } else {
+        specialAlertsFeed.hideAlerts();
+      }
+    },
+    hideAlerts: function () {
+      document.querySelector('.alerts-feed-wrapper').style.display = 'none';
+      document.body.classList.add('no-alerts');
+    },
+    setDismiss: function () {
+      // Attach click functionality.
+      let dismiss = document.querySelectorAll(".newsroom-alert-dismiss");
+      dismiss.forEach(function (el, i) {
+        el.addEventListener("click", function () {
+          let alertId = el.getAttribute("data-alert-id");
+          document.getElementById("newsroom-alert-" + alertId).classList.add("hidden");
+          specialAlertsFeed.setCookie('howard-newsroom-alerts--' + alertId, 'Howard Dig Alert ' + alertId + ' dismissed.', 1);
+        });
+      });
+    },
+    setCookie: function (key, value, days) {
+      let expires = new Date();
+      if (days) {
+        expires.setTime(expires.getTime() + (days * 24 * 60 * 60 * 1000));
+        document.cookie = key + '=' + value + ";expires=" + expires.toUTCString();
+      } else {
+        document.cookie = key + '=' + value + ";expires=Fri, 30 Dec 9999 23:59:59 GMT;";
+      }
+    },
+    getCookie: function (key) {
+      let keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
+      return keyValue ? keyValue[2] : null;
     }
-  };
-})(jQuery, Drupal, drupalSettings);
+  }
+  specialAlertsFeed.loadEventData();
+})();
